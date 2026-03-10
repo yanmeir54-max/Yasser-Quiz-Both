c0e02dfe6a64e3a17a7424ab2305d7170193c5e3
yanmeir54-max <yanmeir54@gmail.com>
2026-03-10 17:37:53 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -550,7 +550,7 @@ async def generate_zidni_card(user_id: int, bot, supabase):
         # 3. جلب ووضع صورة البروفايل
         profile_circle = await get_profile_img(bot, user_id)
         if profile_circle:
-            template.paste(profile_circle, (82, 65), profile_circle)
+            template.paste(profile_circle, (82, 62), profile_circle)
 
         # 4. تجهيز البيانات النصية
         name = str(user_db.get("user_name", "غير معروف"))[:20]
