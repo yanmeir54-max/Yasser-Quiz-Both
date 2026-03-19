3131d6f5386a19707e4db02ff423ce75ecc9821b
yanmeir54-max <yanmeir54@gmail.com>
2026-03-20 01:56:57 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -3983,7 +3983,7 @@ async def run_universal_logic(chat_id, questions, quiz_data, owner_name, engine_
             "is_global": (engine_type == "bot"),
             "is_active": True,
             "category_name": main_cat
-            "quiz_type": "private" 
+            "quiz_type": private, 
         }).execute()
         
         if quiz_reg.data:
